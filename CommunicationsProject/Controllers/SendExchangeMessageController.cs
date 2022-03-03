83a0bb9b03648aaf185032c0ca7be4849c2330b1
Новиков Константин Дмитриевич <ultimatedoker@gmail.com>
2022-03-03 15:50:03 +0300

    Added a few clients with different exchanges;

---
+++ CommunicationsProject/Controllers/SendExchangeMessageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace CommunicationsProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SendExchangeMessageController : ControllerBase
+    {
+        [HttpPost]
+        public string Post(string message, string exchangeName)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: exchangeName,
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                return "Complete with message = " + message;
+            }
+        }
+    }
+}
---
+++ ThirdReceiver/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ThirdReceiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "group-two", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "group-two",
+                                  routingKey: "");
+
+                Console.WriteLine(" [3] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [3] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
