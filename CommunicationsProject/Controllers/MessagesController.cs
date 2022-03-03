7ffa7642fc3b77e52292d2a971c68fa559abd3b5
Новиков Константин Дмитриевич <ultimatedoker@gmail.com>
2022-03-03 14:45:51 +0300

    Добавьте файлы проекта.

---
+++ CommunicationsProject/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace CommunicationsProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpPost]
+        public string Post()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "testqueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var data = channel.BasicGet("testqueue", true);
+                if (data != null)
+                    return "Recieved message: " + Encoding.UTF8.GetString(data.Body.ToArray());
+                return "No Messages";
+
+            }
+        }
+    }
+}
---
+++ CommunicationsProject/Controllers/SendMessageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace CommunicationsProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SendMessageController : ControllerBase
+    {
+        [HttpPost]
+        public string Post(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "testqueue",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "testqueue",
+                                     basicProperties: null,
+                                     body: body);
+
+                return "Complete with message = " + message;
+            }
+        }
+    }
+}
